02977daf10d1c50fefeb976a1088c2fc9a22d0b2
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-12-29 22:36:13 +0300

    step#2

--- src/js/Team.js
+++ src/js/Team.js
@@ -2,31 +2,25 @@
 // eslint-disable-next-line import/prefer-default-export
 export class Team {
   constructor() {
-    Team.m.members = [];
-    // this.last = 0;
-    Team.m.last = 0;
+    this.members = [];
+    this.last = 0;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   add(obj) {
-    Team.m.members.push(obj);
-    // this.last++;
-    Team.m.last++;
+    this.members.push(obj);
+    this.last++;
   }
 
-  static m() {}
-
-  // eslint-disable-next-line class-methods-use-this
   *[Symbol.iterator]() {
-    // метод должен вернуть объект с методом next()
+    const { members, last } = this;
     let current = 0;
-    return {
-      next() {
+    // return {
+      // next() {
         // console.log(current, Team.m.last);
-      if (current < Team.m.last) {
-        const x = Team.m.members[current++];
+      if (current < this.last) {
+        const x = this.members[current++];
         yield x;
       };
-    };
-  }
+    // };
+  // }
 }
